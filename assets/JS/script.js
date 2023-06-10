a2a0b93a99eff4b071e9a1ddc6f9e68b0d16758c
Melissa <melissakalish2@gmail.com>
2023-06-10 12:22:07 -0500

    progress work commit

---
+++ assets/JS/script.js
@@ -0,0 +1,37 @@
+// Placeholder and timer
+let questionsNumber = 0;
+let seconds = 45;
+
+ timer = 
+
+
+
+
+
+
+
+
+
+//Questions and Answers, setting up the variables
+var questions = [
+    {
+        question: "A randon number can be created by ________________",
+        answers: ["Math.random", "math./random", "Math.floor", "Math.Floor"],
+        correctAnswer: 1
+    },
+    {
+        question: "How do you write an IF statement in JavaScript?",
+        answers: ["if i ==5 then", "if i = 5 then", "if (i === 5)", "if i = 5"],
+        correctAnswer: 2
+    },
+    {
+        question: "String values must be enclosed in ___________ whn being assigned to variables",
+        answers: ["quotations", "commas", "parenthesis", "curly brackets"], 
+        correctAnswer: 1
+    },
+    {
+        question: "Inside the HTML document, where do you place the JavaScript code?",
+        answers: ["in the <footer> element", "inside the <script> element", "inside the <link> element", "inside the <header> element"],
+        correctAnswer: 2
+    },
+]
